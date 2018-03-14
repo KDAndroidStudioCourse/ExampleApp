ac81a11301cb002b512944c758d4cd2c5d31e9f6
Karl Darragjati <karl.darragjati@gmail.com>
2018-03-14 02:08:06 +0100

    02. Buttons And TextFields

--- app/src/main/java/com/cj/darra/exampleapp/MainActivity.java
+++ app/src/main/java/com/cj/darra/exampleapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.cj.darra.exampleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void  clickFunction(View view) {
+        EditText usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+        EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
+        Log.i("Info", "Button pressed!");
+
+        Log.i("Values", "Username: " + usernameEditText.getText().toString() + ", Password: " + passwordEditText.getText().toString());
     }
 }

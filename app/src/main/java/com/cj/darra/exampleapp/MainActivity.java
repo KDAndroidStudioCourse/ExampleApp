fffe25dd9119d1f39cf810416a8d4fe28d1fb18f
Karl Darragjati <karl.darragjati@gmail.com>
2018-03-18 20:48:34 +0100

    05. Fading Animation

--- app/src/main/java/com/cj/darra/exampleapp/MainActivity.java
+++ app/src/main/java/com/cj/darra/exampleapp/MainActivity.java
@@ -4,31 +4,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import java.text.DecimalFormat;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    boolean bart = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("Currency Converter");
     }
 
     public void  clickFunction(View view) {
-        EditText amountEditText = (EditText) findViewById(R.id.amountEditText);
+        Log.i("Info", "Image pressed!");
 
-        Log.i("Info", "Button pressed!");
+        ImageView simpson = (ImageView) findViewById(R.id.simpsonImageView);
 
-        Log.i("Amount in $", "" + amountEditText.getText().toString());
+        simpson.animate().alpha(0).setDuration(2000);
 
-        double amount = Double.parseDouble(amountEditText.getText().toString()) * 0.808668931;
+        if(bart) {
+            simpson.setImageResource(R.drawable.homer);
+            bart = false;
+        } else {
+            simpson.setImageResource(R.drawable.bart);
+            bart = true;
+        }
 
-        Log.i("Amount in €", "" + amount);
-
-        Toast.makeText(this, "It is " + String.format("%.2f", amount) + "€!", Toast.LENGTH_LONG).show();
+        simpson.animate().alpha(1).setDuration(2000);
     }
 }

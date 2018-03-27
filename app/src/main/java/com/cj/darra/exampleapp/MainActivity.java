d2aab253458285cd4b7c7e738fe762995d93485f
Karl Darragjati <karl.darragjati@gmail.com>
2018-03-27 14:47:39 +0200

    07. Video

--- app/src/main/java/com/cj/darra/exampleapp/MainActivity.java
+++ app/src/main/java/com/cj/darra/exampleapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
     boolean bart = true;
@@ -13,13 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Video Demo");
+
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.demovideo);
+
+        MediaController mediaController = new MediaController(this);
+
+        mediaController.setAnchorView(videoView);
+
+        videoView.setMediaController(mediaController);
+
+        videoView.start();
     }
 
     public void  clickFunction(View view) {
         Log.i("Info", "Image pressed!");
-
-        ImageView simpson = (ImageView) findViewById(R.id.simpsonImageView);
-
-        simpson.animate().scaleX(0.5f).scaleY(0.5f).setDuration(2000);
     }
 }

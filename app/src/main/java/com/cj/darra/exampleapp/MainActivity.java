756b8164688bac0a608893a163574ae302fd27ef
Karl Darragjati <karl.darragjati@gmail.com>
2018-03-29 00:34:19 +0200

    14. Timers In Android

--- app/src/main/java/com/cj/darra/exampleapp/MainActivity.java
+++ app/src/main/java/com/cj/darra/exampleapp/MainActivity.java
@@ -1,18 +1,13 @@
 package com.cj.darra.exampleapp;
 
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.SeekBar;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-
-import static java.util.Arrays.asList;
+import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,71 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("Times Table");
+        setTitle("Timers");
 
-        final SeekBar seekBar = findViewById(R.id.seekBar);
+        /*final Handler handler = new Handler();
 
-        final ListView myListView = findViewById(R.id.myListView);
-
-        seekBar.setMax(20);
-        seekBar.setProgress(10);
-
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        Runnable run = new Runnable() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                int min = 1;
-                int progress;
+            public void run() {
+                Log.i("Hey", "A second passed");
 
-                if (i < min) {
-                    progress = min;
-                    seekBar.setProgress(progress);
-                } else {
-                    progress = i;
-                }
+                handler.postDelayed(this, 1000);
+            }
+        };
 
-                final ArrayList<String> numbers = new ArrayList<>();
+        handler.post(run);*/
 
-                for (int j = 1; j<=100; j++) {
-                    numbers.add(Integer.toString(j*progress));
-                }
-
-                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, numbers);
-
-                myListView.setAdapter(arrayAdapter);
-                myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                        Toast.makeText(MainActivity.this,"" + numbers.get(i),Toast.LENGTH_LONG).show();
-                    }
-                });
+        new CountDownTimer(10000, 1000) {
+            public void onTick(long millisecondsUntilDone) {
+                Log.i("Seconds left", "" + millisecondsUntilDone/1000);
             }
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
+            public void onFinish() {
+                Log.i("We are done", "");
             }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-        final ArrayList<String> numbers = new ArrayList<>();
-
-        for (int j = 1; j<=100; j++) {
-            numbers.add(Integer.toString(j*10));
-        }
-
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, numbers);
-
-        myListView.setAdapter(arrayAdapter);
-        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(MainActivity.this,"" + numbers.get(i),Toast.LENGTH_LONG).show();
-            }
-        });
+        }.start();
     }
 
     public void  play(View view) {

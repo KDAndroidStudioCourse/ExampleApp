7eea97da8ce3b291ff555ccc1ec854a98724683e
Karl Darragjati <karl.darragjati@gmail.com>
2018-03-28 17:19:31 +0200

    12. List Views

--- app/src/main/java/com/cj/darra/exampleapp/MainActivity.java
+++ app/src/main/java/com/cj/darra/exampleapp/MainActivity.java
@@ -1,34 +1,41 @@
 package com.cj.darra.exampleapp;
 
-import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import static java.util.Arrays.asList;
 
 public class MainActivity extends AppCompatActivity {
-    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("Basic Phrases");
+        setTitle("List Views");
+
+        ListView myListView = (ListView) findViewById(R.id.myListView);
+
+        final ArrayList<String> myFamily = new ArrayList<String>(asList("Jul", "Viki", "Viktore", "Gjergj", "Karl"));
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, myFamily);
+        myListView.setAdapter(arrayAdapter);
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this,"" + myFamily.get(i),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     public void  play(View view) {
-        Button buttonPressed = (Button) view;
 
-        Log.i("Tag", "" + buttonPressed.getTag().toString());
-
-        if (mediaPlayer!= null) {
-            mediaPlayer.release();
-            mediaPlayer= null;
-        }
-
-        mediaPlayer = MediaPlayer.create(this, getResources().getIdentifier("" + buttonPressed.getTag().toString(), "raw", getPackageName()));
-
-        mediaPlayer.start();
     }
 }

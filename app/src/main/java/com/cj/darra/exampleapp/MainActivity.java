534a31f7b1e79bcd74320ceeec548dd5fb455fef
Karl Darragjati <karl.darragjati@gmail.com>
2018-03-28 17:45:38 +0200

    13. Times Table App

--- app/src/main/java/com/cj/darra/exampleapp/MainActivity.java
+++ app/src/main/java/com/cj/darra/exampleapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -19,18 +20,69 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("List Views");
+        setTitle("Times Table");
 
-        ListView myListView = (ListView) findViewById(R.id.myListView);
+        final SeekBar seekBar = findViewById(R.id.seekBar);
 
-        final ArrayList<String> myFamily = new ArrayList<String>(asList("Jul", "Viki", "Viktore", "Gjergj", "Karl"));
+        final ListView myListView = findViewById(R.id.myListView);
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, myFamily);
+        seekBar.setMax(20);
+        seekBar.setProgress(10);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int min = 1;
+                int progress;
+
+                if (i < min) {
+                    progress = min;
+                    seekBar.setProgress(progress);
+                } else {
+                    progress = i;
+                }
+
+                final ArrayList<String> numbers = new ArrayList<>();
+
+                for (int j = 1; j<=100; j++) {
+                    numbers.add(Integer.toString(j*progress));
+                }
+
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, numbers);
+
+                myListView.setAdapter(arrayAdapter);
+                myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        Toast.makeText(MainActivity.this,"" + numbers.get(i),Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        final ArrayList<String> numbers = new ArrayList<>();
+
+        for (int j = 1; j<=100; j++) {
+            numbers.add(Integer.toString(j*10));
+        }
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, numbers);
+
         myListView.setAdapter(arrayAdapter);
         myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(MainActivity.this,"" + myFamily.get(i),Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this,"" + numbers.get(i),Toast.LENGTH_LONG).show();
             }
         });
     }

3b02644756c8136c6f0b59d4ebbb32d7950719b9
Karl Darragjati <karl.darragjati@gmail.com>
2018-04-03 18:52:10 +0200

    17. Downloading Web Content

--- app/src/main/java/com/cj/darra/exampleapp/MainActivity.java
+++ app/src/main/java/com/cj/darra/exampleapp/MainActivity.java
@@ -1,136 +1,64 @@
 package com.cj.darra.exampleapp;
 
-import android.os.CountDownTimer;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.GridLayout;
-import android.widget.TextView;
+import android.util.Log;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-    Button goButton, button0, button1, button2, button3;
-    TextView timeTextView, sumTextView, scoreTextView, resultTextView;
-    GridLayout gridLayout;
 
-    Random random;
-    int a, b, maxNumber, maxTime, numberOfAnswers, locationOfCorrectAnswer;
-    int score, numberOfQuestions;
+    public class DownloadTask extends AsyncTask<String, Void, String> {
 
-    ArrayList<Integer> answers;
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data;
+
+                do {
+                    data = reader.read();
+
+                    char current = (char) data;
+
+                    result += current;
+                } while(data != -1);
+
+                return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "Failed";
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("Brain trainer");
+        setTitle("Downloading Web Content");
 
-        goButton = findViewById(R.id.goButton);
+        DownloadTask task = new DownloadTask();
+        String result = null;
 
-        timeTextView = findViewById(R.id.timeTextView);
-        sumTextView = findViewById(R.id.sumTextView);
-        scoreTextView = findViewById(R.id.scoreTextView);
-        resultTextView = findViewById(R.id.resultTextView);
-
-        gridLayout = findViewById(R.id.gridLayout);
-        button0 = findViewById(R.id.answerButton0);
-        button1 = findViewById(R.id.answerButton1);
-        button2 = findViewById(R.id.answerButton2);
-        button3 = findViewById(R.id.answerButton3);
-
-        answers = new ArrayList<>();
-
-        random = new Random();
-
-        maxNumber = 20;
-        maxTime = 30;
-        score = 0;
-        numberOfQuestions = 0;
-
-        numberOfAnswers = gridLayout.getRowCount() * gridLayout.getColumnCount();
-
-        resultTextView.setText("");
-    }
-
-    public void start(View view) {
-        resultTextView.setText("");
-
-        setVisibility(false);
-
-        setUI();
-
-        new CountDownTimer(maxTime*1000+100, 1000) {
-            @Override
-            public void onTick(long l) {
-                timeTextView.setText(String.valueOf(l/1000) + "s");
-            }
-
-            @Override
-            public void onFinish() {
-                resultTextView.setText("You got " + Integer.toString(score) + " right answers out of " + Integer.toString(numberOfQuestions));
-                setVisibility(true);
-            }
-        }.start();
-    }
-
-    public void answer(View view) {
-        if(Integer.toString(locationOfCorrectAnswer).equals(view.getTag().toString())) {
-            resultTextView.setText("Correct!");
-            score++;
-        } else {
-            resultTextView.setText("Wrong! :(");
+        try {
+            result = task.execute("http://www.inf.uniroma3.it/~patrigna/didactic/imp_elab/esempi_html/esempio_1.html").get();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        numberOfQuestions++;
-        answers.clear();
-        setUI();
-    }
-
-    private void setVisibility(Boolean active) {
-        if(active) {
-            timeTextView.setVisibility(View.INVISIBLE);
-            sumTextView.setVisibility(View.INVISIBLE);
-            scoreTextView.setVisibility(View.INVISIBLE);
-            gridLayout.setVisibility(View.INVISIBLE);
-
-            goButton.setVisibility(View.VISIBLE);
-        } else {
-            goButton.setVisibility(View.INVISIBLE);
-
-            timeTextView.setVisibility(View.VISIBLE);
-            sumTextView.setVisibility(View.VISIBLE);
-            scoreTextView.setVisibility(View.VISIBLE);
-            gridLayout.setVisibility(View.VISIBLE);
-        }
-    }
-
-    private void setUI() {
-        a = random.nextInt(maxNumber + 1);
-        b = random.nextInt(maxNumber + 1);
-
-        sumTextView.setText(Integer.toString(a) + " + " + Integer.toString(b));
-        scoreTextView.setText(Integer.toString(score) + " out of " + Integer.toString(numberOfQuestions));
-
-        locationOfCorrectAnswer = random.nextInt(numberOfAnswers);
-
-        for (int i = 0; i < numberOfAnswers; i++) {
-            if(i == locationOfCorrectAnswer) {
-                answers.add(a+b);
-            } else {
-                int wrongAnswer = random.nextInt(maxNumber*2+1);
-                while (wrongAnswer==a+b) {
-                    wrongAnswer = random.nextInt(maxNumber*2+1);
-                }
-                answers.add(wrongAnswer);
-            }
-        }
-
-        button0.setText(Integer.toString(answers.get(0)));
-        button1.setText(Integer.toString(answers.get(1)));
-        button2.setText(Integer.toString(answers.get(2)));
-        button3.setText(Integer.toString(answers.get(3)));
+        Log.i("Result", "" + result);
     }
 }

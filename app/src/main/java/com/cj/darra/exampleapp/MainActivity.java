64739f38f4307cb1a0adc3c572ad87609eaa9fc2
Karl Darragjati <karl.darragjati@gmail.com>
2018-04-03 18:13:05 +0200

    16. Brain Trainer App

--- app/src/main/java/com/cj/darra/exampleapp/MainActivity.java
+++ app/src/main/java/com/cj/darra/exampleapp/MainActivity.java
@@ -1,105 +1,136 @@
 package com.cj.darra.exampleapp;
 
-import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.SeekBar;
+import android.widget.GridLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    SeekBar seekBar;
-    TextView textView;
-    Button button;
-    Boolean active = false;
-    CountDownTimer timer;
+import java.util.ArrayList;
+import java.util.Random;
 
-    int max = 600;
-    int start = 30;
+public class MainActivity extends AppCompatActivity {
+    Button goButton, button0, button1, button2, button3;
+    TextView timeTextView, sumTextView, scoreTextView, resultTextView;
+    GridLayout gridLayout;
+
+    Random random;
+    int a, b, maxNumber, maxTime, numberOfAnswers, locationOfCorrectAnswer;
+    int score, numberOfQuestions;
+
+    ArrayList<Integer> answers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("Egg timer");
+        setTitle("Brain trainer");
 
-        seekBar = findViewById(R.id.seekBar);
+        goButton = findViewById(R.id.goButton);
 
-        textView = findViewById(R.id.timer);
+        timeTextView = findViewById(R.id.timeTextView);
+        sumTextView = findViewById(R.id.sumTextView);
+        scoreTextView = findViewById(R.id.scoreTextView);
+        resultTextView = findViewById(R.id.resultTextView);
 
-        seekBar.setMax(max);
-        seekBar.setProgress(start);
+        gridLayout = findViewById(R.id.gridLayout);
+        button0 = findViewById(R.id.answerButton0);
+        button1 = findViewById(R.id.answerButton1);
+        button2 = findViewById(R.id.answerButton2);
+        button3 = findViewById(R.id.answerButton3);
 
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                progress(i);
-            }
+        answers = new ArrayList<>();
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
+        random = new Random();
 
-            }
+        maxNumber = 20;
+        maxTime = 30;
+        score = 0;
+        numberOfQuestions = 0;
 
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
+        numberOfAnswers = gridLayout.getRowCount() * gridLayout.getColumnCount();
 
-            }
-        });
+        resultTextView.setText("");
     }
 
-    public void  play(View view) {
-        button = (Button) view;
-        if(!active) {
-            timer = new CountDownTimer(seekBar.getProgress() * 1000 + 100, 1000) {
-                public void onTick(long millisecondsUntilDone) {
-                    int i = seekBar.getProgress() - 1;
-                    seekBar.setProgress(i);
+    public void start(View view) {
+        resultTextView.setText("");
 
-                    progress(i);
-                }
+        setVisibility(false);
 
-                public void onFinish() {
-                    MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.airhorn);
-                    mediaPlayer.start();
-                    reset();
-                }
-            };
+        setUI();
 
-            timer.start();
+        new CountDownTimer(maxTime*1000+100, 1000) {
+            @Override
+            public void onTick(long l) {
+                timeTextView.setText(String.valueOf(l/1000) + "s");
+            }
 
-            seekBar.setEnabled(false);
+            @Override
+            public void onFinish() {
+                resultTextView.setText("You got " + Integer.toString(score) + " right answers out of " + Integer.toString(numberOfQuestions));
+                setVisibility(true);
+            }
+        }.start();
+    }
 
-            button.setText("Stop");
-
-            active = true;
+    public void answer(View view) {
+        if(Integer.toString(locationOfCorrectAnswer).equals(view.getTag().toString())) {
+            resultTextView.setText("Correct!");
+            score++;
         } else {
-            reset();
+            resultTextView.setText("Wrong! :(");
+        }
+
+        numberOfQuestions++;
+        answers.clear();
+        setUI();
+    }
+
+    private void setVisibility(Boolean active) {
+        if(active) {
+            timeTextView.setVisibility(View.INVISIBLE);
+            sumTextView.setVisibility(View.INVISIBLE);
+            scoreTextView.setVisibility(View.INVISIBLE);
+            gridLayout.setVisibility(View.INVISIBLE);
+
+            goButton.setVisibility(View.VISIBLE);
+        } else {
+            goButton.setVisibility(View.INVISIBLE);
+
+            timeTextView.setVisibility(View.VISIBLE);
+            sumTextView.setVisibility(View.VISIBLE);
+            scoreTextView.setVisibility(View.VISIBLE);
+            gridLayout.setVisibility(View.VISIBLE);
         }
     }
 
-    public void progress(int secondsLeft) {
-        int minutes = secondsLeft / 60;
-        int seconds = secondsLeft % 60;
+    private void setUI() {
+        a = random.nextInt(maxNumber + 1);
+        b = random.nextInt(maxNumber + 1);
 
-        if(seconds < 10) {
-            textView.setText(Integer.toString(minutes) + ":0" + Integer.toString(seconds));
-        } else {
-            textView.setText(Integer.toString(minutes) + ":" + Integer.toString(seconds));
+        sumTextView.setText(Integer.toString(a) + " + " + Integer.toString(b));
+        scoreTextView.setText(Integer.toString(score) + " out of " + Integer.toString(numberOfQuestions));
+
+        locationOfCorrectAnswer = random.nextInt(numberOfAnswers);
+
+        for (int i = 0; i < numberOfAnswers; i++) {
+            if(i == locationOfCorrectAnswer) {
+                answers.add(a+b);
+            } else {
+                int wrongAnswer = random.nextInt(maxNumber*2+1);
+                while (wrongAnswer==a+b) {
+                    wrongAnswer = random.nextInt(maxNumber*2+1);
+                }
+                answers.add(wrongAnswer);
+            }
         }
-    }
 
-    public void reset() {
-        timer.cancel();
-
-        textView.setText("0:" + Integer.toString(start));
-        seekBar.setProgress(start);
-        seekBar.setEnabled(true);
-
-        button.setText("Start");
-
-        active = false;
+        button0.setText(Integer.toString(answers.get(0)));
+        button1.setText(Integer.toString(answers.get(1)));
+        button2.setText(Integer.toString(answers.get(2)));
+        button3.setText(Integer.toString(answers.get(3)));
     }
 }
